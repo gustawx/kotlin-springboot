754d05dfa3d47ad6ab7cd45399bf2b5a55def316
Kustosz <sebastian.kustosz.ext@siemens.com>
2020-08-28 15:48:21 +0200

    first commit

---
+++ src/main/kotlin/com/kotlinspring/theater/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package com.kotlinspring.theater
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(TheaterApplication::class.java)
+	}
+
+}
